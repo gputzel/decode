5eec4a6bdbb938c18afb8d8404af4a76b9797049
Gregory G. Putzel <gregputzel@gmail.com>
2017-03-18 14:32:10 -0400

    Added parameter k to ll function

--- likelihood.py
+++ likelihood.py
@@ -1,6 +1,6 @@
 import math
 #Log-likelihood
-def ll(ciphertext,perm,mat):
+def ll(ciphertext,perm,mat,k):
     s=0.0
     for i in range(len(ciphertext)-(k-1)):
         kmer = tuple([perm[c] for c in ciphertext[i:i+k]])
