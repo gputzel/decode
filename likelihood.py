f0b96aa0d4921e161eee9bb1a83846442f7f63b2
Gregory G. Putzel <gregputzel@gmail.com>
2017-03-18 14:26:39 -0400

    Remove default value for k - this should never be necessary for this function

--- likelihood.py
+++ likelihood.py
@@ -1,6 +1,6 @@
 import math
 #Log-likelihood
-def ll(ciphertext,perm,mat,k=2):
+def ll(ciphertext,perm,mat):
     s=0.0
     for i in range(len(ciphertext)-(k-1)):
         kmer = tuple([perm[c] for c in ciphertext[i:i+k]])
