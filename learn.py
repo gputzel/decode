6086576b5475bc923b47e6a9af7da5b6a2cf8800
Gregory G. Putzel <gregputzel@gmail.com>
2017-03-18 13:58:57 -0400

    Add new learnMatrix function that works with arbitrary k (kmer length)

--- learn.py
+++ learn.py
@@ -1,5 +1,30 @@
+import itertools
+
 #Learn the transition matrix from a training text
-def learnMatrix(trainingText,alphabet):
+#New version, which works with arbitrary kmers
+def LearnMatrix(trainingText,alphabet,k=2):
+    #The transition matrix is represented by a dictionary whose elements are kmers from the target alphabet
+    mat={}
+    #We will start with each matrix element equal to 0.1
+    #This is to avoid having matrix elements equal to zero, because that produces likelihoods of zero
+    #and we always work with the log of the likelihood.
+    for kmer in itertools.product(*[alphabet]*k):
+        mat[kmer] = 0.1
+    for i in range(len(trainingText)-(k-1)):
+        kmer = tuple(trainingText[i:i+k])
+        mat[kmer] = mat[kmer]+1.0
+    #Now normalize the transition matrix so that the elements in any given row add to 1.0
+    #(Because they are conditional probabilities)
+    for kmer in itertools.product(*[alphabet]*(k-1)):
+        s=0.0
+        for c in alphabet:
+            s = s + mat[kmer + (c,)]
+        for c in alphabet:
+            mat[kmer + (c,)] = mat[kmer + (c,)]/s
+    return mat
+
+#Learn the transition matrix from a training text
+def learnMatrixOld(trainingText,alphabet):
 	#The transition matrix is represented by a dictionary whose keys are pairs of elements of target alphabet
 	mat={}
 	#We will start with each matrix element equal to 0.1
