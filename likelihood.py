744cbdf148fbe8a40aaa27ec03f03f7f53f379e7
Gregory G. Putzel <gregputzel@gmail.com>
2017-03-18 14:15:00 -0400

    Rewrite ll (log likelihood) function to work with arbitrary values of k (kmer length)

--- likelihood.py
+++ likelihood.py
@@ -1,10 +1,8 @@
 import math
 #Log-likelihood
-def ll(ciphertext,perm,mat):
-	s=0.0
-	c1 = ciphertext[0]
-	for c2 in ciphertext[1:]:
-		#print math.log(mat[(perm[c1],perm[c2])])
-		s = s + math.log(mat[(perm[c1],perm[c2])])
-		c1=c2
-	return s
+def ll(ciphertext,perm,mat,k=2):
+    s=0.0
+    for i in range(len(ciphertext)-(k-1)):
+        kmer = tuple([perm[c] for c in ciphertext[i:i+k]])
+        s = s + math.log(mat[kmer])
+    return s
