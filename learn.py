6717df4530461d6dc289664313d021731b81d0df
Gregory G. Putzel <gregputzel@gmail.com>
2017-03-21 22:53:40 -0400

    Clean up code

--- learn.py
+++ learn.py
@@ -21,30 +21,6 @@ def learnMatrix(trainingText,alphabet,k):
             mat[begin][end] = mat[begin][end]/s
     return mat
 
-#Learn the transition matrix from a training text
-#New version, which works with arbitrary kmers
-def learnMatrixOld(trainingText,alphabet,k):
-    #The transition matrix is represented by a dictionary whose elements are kmers from the target alphabet
-    mat={}
-    #We will start with each matrix element equal to a small "pseudocount"
-    pseudocount = 0.0001
-    #This is to avoid having matrix elements equal to zero, because that produces likelihoods of zero
-    #and we always work with the log of the likelihood.
-    for kmer in itertools.product(*[alphabet]*k):
-        mat[kmer] = pseudocount
-    for i in range(len(trainingText)-(k-1)):
-        kmer = tuple(trainingText[i:i+k])
-        mat[kmer] = mat[kmer]+1.0
-    #Now normalize the transition matrix so that the elements in any given row add to 1.0
-    #(Because they are conditional probabilities)
-    for kmer in itertools.product(*[alphabet]*(k-1)):
-        s=0.0
-        for c in alphabet:
-            s = s + mat[kmer + (c,)]
-        for c in alphabet:
-            mat[kmer + (c,)] = mat[kmer + (c,)]/s
-    return mat
-
 def learnVector(trainingText, alphabet):
     '''Compute the single-character distribution of trainingText.'''
     # Initialize dictionary of (character, probability) pairs.
