592e8847af041e0d6c628459fac4ef45ea5f141b
Gregory G. Putzel <gregputzel@gmail.com>
2017-04-01 14:21:40 -0400

    Change likelihood.py

--- likelihood.py
+++ likelihood.py
@@ -25,6 +25,21 @@ def plaintextLL(plaintext,mat,k):
             s = s - 10.0
     return s
 
+#Log-likelihood of a proposed plaintext
+def plaintextLL2(plaintext,mat,k):
+    s=0.0
+    for i in range(len(plaintext)-(k-1)):
+        kmer = plaintext[i:i+k]
+        begin = kmer[:k-1]
+        end = kmer[1:]
+        print begin,end
+        if (begin in mat) and (end in mat[begin]):
+            print "delta = ", math.log(mat[begin][end])
+            s = s + math.log(mat[begin][end])
+        else: #Assign it some low likelihood
+            print "minus 10"
+            s = s - 10.0
+    return s
 
 ##Log-likelihood - hard-coded version for k=1
 def ll_k1(ciphertext,perm,mat):
